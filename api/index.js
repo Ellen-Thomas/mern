c5699cb4dc4fa373c13dc5be04ba1b4900c43393
Ellen-Thomas <ellenthomas686@gmail.com>
2025-01-28 16:14:38 +0600

    api

--- api/index.js
+++ api/index.js
@@ -6,21 +6,22 @@ import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose
-.connect(process.env.MONGO)
-.then(() => {
+  .connect(process.env.MONGO)
+  .then(() => {
     console.log('Connected to MongoDB!');
-})
-.catch((err) => {
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log('Server is running on port 3000');
 });
 
-app.get('/test' (req, res)=> {
-    res.send('Home World!');
-}
-);
+// Fixing missing comma in route definition
+app.get('/test', (req, res) => {
+  res.send('Home World!');
+});
+
